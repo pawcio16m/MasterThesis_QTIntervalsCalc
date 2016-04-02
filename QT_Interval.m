374c35cf1759c8288866c9a92500d32849ccdee2
Paweł <pawcio16m@gmail.com>
2016-04-02 20:53:15 +0200

    First step to QRS Onset detection

--- QT_Interval.m
+++ QT_Interval.m
@@ -4,10 +4,7 @@ clear
 close all
 clc
 
-
-%reading signal from file PTB database from Physionet.org
-
-[time,signal] = rdsamp('ptbdb/patient002/s0015lre.hea');
+display('QT analysis program starts');
 
 
 %Global variables
@@ -16,40 +13,49 @@ drainNumber = 1;
 Fc_Hp = 3;  %Hz
 Fc_Lp = 40; %Hz
 Fs = 1000;  %Hz
-numberOfSamples = 100000;
 Lp = myfilterdesign(1,Fs,Fc_Lp,30,'Blackman');
+numberOfSamples = 1000;
+patientNumber = 3;
 
 
-%signal operation
+%reading signal from file PTB database from Physionet.org
 
-signalFromOneDrain = signal(:,drainNumber);
-signalSample = signalFromOneDrain(1:numberOfSamples);
-timeSample = time(1:numberOfSamples);
-
-
+% [time,signal] = rdsamp('ptbdb/patient002/s0015lre.hea');
 %filepath for patient 
-% patientNumber = 150;
-% filepath = strcat('ptbdb/patient',sprintf('%03d',patientNumber));
-% 
-% 
-% %choose all .dat files in the patient folder
-% datFiles = dir(strcat(filepath,'/*.hea'));
+display(sprintf('Load signal for patient %d from PTB database at Physionet.org',patientNumber));
+filepath = strcat('ptbdb/patient',sprintf('%03d',patientNumber));
+%choose first .dat file in the patient folder
+datFiles = dir(strcat(filepath,'/*.hea'));
+filepath = strcat(filepath,'/',datFiles(1).name);
+[time,signal] = rdsamp(filepath); 
+%choose all .dat files in the patient folder
 % length(datFiles)
 % for i=1:length(datFiles)
 %     [time,signal,Fs] = rdsamp(datFiles(i).name);   
 % end
 
 
+%signal operation
+
+numberOfSamples = length(signal);
+signalFromOneDrain = signal(:,drainNumber);
+signalSample = signalFromOneDrain(1:numberOfSamples);
+timeSample = time(1:numberOfSamples);
+
+
 %show raw signal
+
 figure(1);
 plot(timeSample,signalSample);
 title('Raw signal');
 xlabel('time [s]');
 ylabel('Amplitude');
+legend('Signal');
 
 
 %filtering
 
+display(sprintf('Signal filtering highpass Butterworth filter with F_c = %d Hz and lowpass FIR filter with Blackman window F_c = %d Hz',Fc_Hp,Fc_Lp));
 %High-pass filtering - IIR Butterworth 1st. order, Fc = 3 Hz 
 
 [B,A] = butter(1,3/(Fs/2),'high');    
@@ -59,6 +65,7 @@ plot(timeSample,signalFiltered);
 title('Signal after high-pass filter');
 xlabel('time [s]');
 ylabel('Amplitude');
+legend('Signal');
 
 %Low-pass filtering - Blackman window, Fc = 40 Hz
 
@@ -68,20 +75,40 @@ plot(timeSample,signalFiltered);
 title('Signal after low-pass filter');
 xlabel('time [s]');
 ylabel('Amplitude');
+legend('Signal');
 
 
 %PanTompkins algorithm
+
+display('Pan Tompkins algorithm to find QRS complex and R peaks');
+tic
 [WaveAmpl,R_index,delay] = pan_tompkin(signalFiltered,Fs,0);
+toc
+display(sprintf('%d R peaks detected in signal',length(R_index)));
 figure(4);
 plot(timeSample,signalFiltered,'b',timeSample(R_index),signalFiltered(R_index),'xr');
-title(sprintf('Signal with QRS detection'));
+title('Signal with R peaks detection');
 xlabel('time [s]');
 ylabel('Amplitude');
 legend('signal','R-peak');
 
+
 %QRS_Onset detection
 
+display('QRS Onset finder algorithm');
+tic
+[QRS_Onset,newR_Peak] = qrsOnset(signalFiltered,1000,R_index);
+toc
+figure(5)
+plot(time,signalFiltered,'b',time(newR_Peak),signalFiltered(newR_Peak),'or',time(QRS_Onset),signalFiltered(QRS_Onset),'og');
+title('Signal with new R peaks detection and QRS Onset');
+xlabel('time [s]');
+ylabel('Amplitude');
+legend('Signal','R-Peak','QRS Onset');
+
+
 %T_End detection
+
 
 %QT_calculation
 
@@ -89,5 +116,7 @@ legend('signal','R-peak');
 %QT interval statistics
 
 
+%end 
+display('Program exited');
 
 
---
+++ qrsOnset.m
@@ -0,0 +1,66 @@
+function [ qrsOnsetIndex , newR_Peak] = qrsOnset( signal,Fs,R_Peak )
+%function [ qrsOnsetIndex , newR_Peak] = qrsOnset( signal,Fs,R_Peak )
+%The function calculate QRS Onset index in filtered ECG signal
+%
+%Inputs:
+%   - signal - filtered ECG signal
+%   - Fs - frequency sampling in Hz
+%   - R_Peak - matrix (1xN) with R Peak indexes 
+%
+%Outputs:
+%   - qrsOnsetIndex - matrix (1xN) with QRS Onset indexes
+%   - newR_Peak - matrix (1xN) with new R Peak indexes
+%   Detailed explanation goes here
+%
+%  
+  
+    %QRS_Onset detection
+    
+    %set basic const values
+    detectedR_Peaks = length(R_Peak)-2;
+    maxDelay = round(0.02*Fs); 
+    fitSteps = round(maxDelay /2); 
+    correction = 0;
+    
+    %preallocate memory for array
+    newR_Peak = zeros(1,detectedR_Peaks);
+    xfit = zeros(1,fitSteps);   
+    QRS_Onset = zeros(1,detectedR_Peaks);
+    
+    %find local minima in ecg signal
+    %invert data to calculate local minima
+    data = 1.01*max(signal) - signal;
+    %calculate only important minima in 0.05Fs neighbourhood
+    [pks,loc] = findpeaks(data,'minpeakdistance',round(0.05*Fs));
+    %go for each R Peak in signal
+    for i=2:length(R_Peak)-1
+        
+        rPeak = R_Peak(i) ;
+        %correct R_Peak detection from pantompkins algorithm
+        %make sure that R Peak is on increasing slope
+     
+        while(signal(rPeak+1)<signal(rPeak) && correction<maxDelay)
+            rPeak = rPeak-1;
+            correction = correction + 1;
+        end
+        
+        correction = 0;
+        newR_Peak(i-1) = rPeak;
+        
+        for j=fitSteps:-1:1
+            xfit((fitSteps+1)-j) = rPeak - j;
+        end    
+        yfit = signal(xfit);
+        coeff = polyfit(xfit,yfit,1);        
+        qrs_onset = round(-coeff(2)/coeff(1));
+        %find the nearest local minimum to calculated value
+        temp = find(loc<qrs_onset);
+        %chcek if this minium is correct - not to far from calculated value
+        if(qrs_onset - loc(temp(end)) > round(0.04*Fs))
+            qrs_onset = loc(temp(end));
+        end
+        QRS_Onset(i-1) = qrs_onset;
+    end
+    qrsOnsetIndex = QRS_Onset;
+end
+
