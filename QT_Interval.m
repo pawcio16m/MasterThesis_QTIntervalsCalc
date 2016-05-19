f9a6073a5a73aa200362cf5330e0393b178b0392
Paweł <pawcio16m@gmail.com>
2016-05-19 21:30:19 +0200

    Update main program

--- QT_Interval.m
+++ QT_Interval.m
@@ -1,10 +1,11 @@
-function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out] = QT_Interval(patientNumber, draw)
-%function [ signalFiltered , R_index, QRS_Onset, QRS_End_out] = QT_Interval( patientNumber, plot )
+function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out, T_End_out, QT_Interval_out] = QT_Interval(patientNumber, draw, formula)
+%function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out, T_End_out, QT_Interval_out] = QT_Interval(patientNumber, draw, formula)
 %The function execute whole program
 %
 %Inputs:
 %   - patientNumber - patient numberr from PTB database
 %   - draw - a flag to plot figures (1- plot, other no plot)
+%   - formula - choose formula to calculate QT interval ('Bazetta', 'Fridericia' or 'Framigham')
 %
 %Outputs:
 %   - signalFiltered - lowpass and highpass filtering aplyied
@@ -12,8 +13,8 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
 %   - QRS_Onset_out - matrix (1xN) with QRS Onset indexes
 %   - QRS_End_out - matrix (1xN) with QRS End indexes
 %   - T_Max_out - matrix (1xN) with T wave max indexes
+%   - T_End_out - matrix (1xN) with T wave end indexes
 
-  
 
     %clear command window and workspace 
 
@@ -32,18 +33,15 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
     Fs = 1000;  %Hz
     Lp = myfilterdesign(1,Fs,Fc_Lp,30,'Blackman');
     filterOrder = 1;
-    % patientNumber = 5;
-
-
-    %reading signal from file PTB database from Physionet.org
-
-    % [time,signal] = rdsamp('ptbdb/patient002/s0015lre.hea');
+  
+    
+    %File reading
     %filepath for patient 
     display(sprintf('Load signal for patient %d from PTB database at Physionet.org',patientNumber));
     filepath = strcat('newptbdb/ptbdb/patient',sprintf('%03d',patientNumber));
     %choose first .dat file in the patient folder
     datFiles = dir(strcat(filepath,'/*.hea'));
-    filepath = strcat(filepath,'/',datFiles(1).name);
+    filepath = strcat(filepath,'/',datFiles(end).name);
     [time,signal] = rdsamp(filepath); 
     %choose all .dat files in the patient folder
     % length(datFiles)
@@ -58,8 +56,6 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
     signalFromOneDrain = signal(:,drainNumber);
     signalSample = signalFromOneDrain(1:numberOfSamples);
     timeSample = time(1:numberOfSamples);
-
-
     %show raw signal
     if(draw == 1)
         figure();
@@ -72,12 +68,9 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
    
 
     %filtering
-
-    display(sprintf('Signal filtering highpass Butterworth filter with Fc = %d Hz and lowpass FIR filter with Blackman window Fc = %d Hz',Fc_Hp,Fc_Lp));
-    
-    
+    tic
+    display(sprintf('Signal filtering highpass Butterworth filter with Fc = %d Hz and lowpass FIR filter with Blackman window Fc = %d Hz',Fc_Hp,Fc_Lp));    
     %High-pass filtering - IIR Butterworth 1st. order, Fc = 3 Hz 
-
     [B,A] = butter(filterOrder,Fc_Hp/(Fs/2),'high');    
     signalFiltered = filter(B,A,signalSample);
     if(draw == 1)
@@ -88,10 +81,7 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
         ylabel('Amplitude');
         legend('Signal');
     end  
-  
-
     %Low-pass filtering - Blackman window, Fc = 40 Hz
-
     signalFiltered = myfilter(signalFiltered,Lp);
     if(draw == 1)
         figure();
@@ -101,7 +91,7 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
         ylabel('Amplitude');
         legend('Signal');
     end
- 
+    toc
 
     %PanTompkins algorithm
 
@@ -163,12 +153,9 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
     %T_Max detection
    
     display('T Max finder algorithm');
-    [T_Max, invert] = tMax(signalFiltered,QRS_End,Fs);    
-    if(invert == true)
-        display('T wave negative');        
-    else
-        display('T wave positive');
-    end
+    tic
+    [T_Max, type] = tMax(signalFiltered,QRS_End,Fs);    
+    toc
     display(sprintf('%d T wave max detected in signal',length(T_Max)));
     
     if(draw == 1)
@@ -184,33 +171,56 @@ function [signalFiltered_out, R_index_out, QRS_Onset_out, QRS_End_out, T_Max_out
     %T_End detection
     
     display('T End finder algorithm');
+    tic
+    T_End = tEnd(signalFiltered,QRS_Onset,T_Max,Fs,type);
+    toc
+    display(sprintf('%0.2f percentage of T wave end detected in signal',((length(T_End)-length(find(T_End==-1)))/length(T_End))*100));
+    if(draw == 1)
+        figure()
+        plot(time,signalFiltered,'b',time(T_End(T_End~=-1)),signalFiltered(T_End(T_End~=-1)),'or');
+        title('Signal with T wave end indexes');
+        xlabel('time [s]');
+        ylabel('Amplitude');
+        legend('Signal','T End');
+    end
+    
 
 
     %Draw all points on one plot
     
-    display('Draw a plot with all detected points');
-    
+    display('Draw a plot with all detected points');    
     figure()
-    plot(time,signalFiltered,'b',time(newR_Peak),signalFiltered(newR_Peak),'or',time(QRS_Onset),signalFiltered(QRS_Onset),'og',time(QRS_End),signalFiltered(QRS_End),'oy',time(T_Max),signalFiltered(T_Max),'ok');
+    plot(time,signalFiltered,'b',time(newR_Peak),signalFiltered(newR_Peak),'or',time(QRS_Onset),signalFiltered(QRS_Onset),'og',time(QRS_End),signalFiltered(QRS_End),'oy',time(T_End(T_End~=-1)),signalFiltered(T_End(T_End~=-1)),'ok');
     title('Signal with R peaks detection QRS Onset and QRS End');
     xlabel('time [s]');
     ylabel('Amplitude');
-    legend('Signal filtered','R-Peak','QRS Onset','QRS End','T Max');
+    legend('Signal filtered','R-Peak','QRS Onset','QRS End','T End');
     
     
     
     %QT_calculation
+    
+    display('Calculate QT intervals');
+    QTinterval = qtCalculation(QRS_Onset, T_End, newR_Peak, Fs, formula);
+    QTinterval = QTinterval(QTinterval~=0);
 
 
     %QT interval statistics
-
+    
+    display('Present QT interval statistics');
+    QT_Interval_statistic = struct('DrainName','I','All_Intervals',length(QTinterval),'Mean',mean(QTinterval),'Median',median(QTinterval),'StandardDeviation',std(QTinterval),'MinValue',min(QTinterval),'MaxValue',max(QTinterval),'Percentage450',length(find(QTinterval>450)));
+    display(QT_Interval_statistic);
+    
 
     %end 
+    
     signalFiltered_out = signalFiltered;
     R_index_out = newR_Peak;
     QRS_Onset_out = QRS_Onset;
     QRS_End_out = QRS_End;
     T_Max_out = T_Max;
+    T_End_out = T_End;
+    QT_Interval_out = QTinterval;
     display('Program exited');
 
 end
--- T_End_detection_draft.m
+++ T_End_detection_draft.m
@@ -7,142 +7,51 @@ load QRS_END;
 load QRS_ONSET;
 load SIGNAL;
 load R_PEAK;
-load TIME
+load TIME;
+load T_MAX;
+type = 'negative';
 
-meanval = mean(signal);
-threshold = ones(1,length(time));
-threshold = threshold.*meanval;
-Fs = 1000;
 figure()
-% plot(time,signal,'b',time(R_Peak),signal(R_Peak),'or',time(QRS_Onset),signal(QRS_Onset),'og',time(QRS_End),signal(QRS_End),'oy',time,threshold,'--g');
-% title('Signal with R peaks detection QRS Onset and QRS End');
-% xlabel('time [s]');
-% ylabel('Amplitude');
-% legend('Signal filtered','R-Peak','QRS Onset','QRS End','Thershold');
-DataInv = 1.01*max(signal) - signal;
-ivert = false;
-startIndexes = [];
-counter_pos = 0;
-for i=1:length(QRS_End)-1
-    zero_index = QRS_End(i)+2;
-    while(signal(zero_index+1)>signal(zero_index))
-        zero_index = zero_index+1;
-    end
-%     startIndexes(i) =  zero_index;
-%     figure()
-%     subplot(211);
-    signal_temp = signal(zero_index:zero_index+round(0.25*Fs));
-    signal_abs = abs(signal_temp);
-    [pks,loc] = findpeaks(signal_abs,'SortStr','descend');
-   
-    amp(i) = signal_temp(loc(1));
-    if(amp(i)>0)
-        counter_pos = counter_pos+1;
-    end
-   
-%     plot(signal_temp);
-%    
-%     subplot(212)
-%     signalCut = DataInv(zero_index:zero_index+round(0.25*Fs));
-%     plot(signalCut);
-%     [pks,ind] = findpeaks(signalCut,'SortStr','descend');
-%     hold on
-%     plot(ind(1),pks(1),'or');
-%     T_Max(i) = ind(1)+zero_index;
-end
-counter_pos
-counter_neg = length(amp) - counter_pos
-plot(time,signal,'b',time(R_Peak),signal(R_Peak),'or',time(QRS_Onset),signal(QRS_Onset),'og',time(QRS_End),signal(QRS_End),'oy',time,threshold,'--g',time(T_Max),signal(T_Max),'ok');
+plot(time,signal,'b',time(R_Peak),signal(R_Peak),'or',time(QRS_Onset),signal(QRS_Onset),'og',time(QRS_End),signal(QRS_End),'oy',time(tmax),signal(tmax),'ok');
 title('Signal with R peaks detection QRS Onset and QRS End');
 xlabel('time [s]');
 ylabel('Amplitude');
-legend('Signal filtered','R-Peak','QRS Onset','QRS End','Thershold','T Max');
-%     plot(signalCut);
-%     title('Signal');
-%     subplot(212)
-%    
-%     signalCut = abs(signalCut);
-% 
-%     plot(signalCut);
-%     title('Abs signal');
-%   
-%     hold on
-% 
-%         
-%         [pks,loc] = findpeaks(abs(signalCut),'NPEAKS',7);
-%         new_pks = [];
-%         new_loc = [];
-% %         max_pks = max(pks);
-% %         for j=1:length(pks)
-% %             if pks(j) > 0.3*max_pks
-% %                 new_pks = [new_pks,pks(j)];
-% %                 new_loc = [new_loc,loc(j)];
-% %             end
-% %         end
-% %         tosort = [new_pks;new_loc];
-%         tosort = [pks;loc];
-%         [val , ind] = sort(tosort(1,:),'descend');
-%         sorted_table = tosort(:,ind);
-%         T_max = [];
-%         if length(sorted_table) > 4
-%             sorted_table = sorted_table(:,[1 2 3 ]);
-%         end
-%         
-%       
-%         amp = sorted_table(1,:);
-%         ind = sorted_table(2,:);  
-%        
-%         plot(ind,amp,'or');
-%     
-% end
-% 
-% % 
-%         new_pks = [];
-%         new_loc = [];
-%         max_pks = max(pks);
-%         for j=1:length(pks)
-%             if pks(j) > 0.5*max_pks
-%                 new_pks = [new_pks,pks(j)];
-%                 new_loc = [new_loc,loc(j)];
-%             end
-%         end
-%         tosort = [new_pks;new_loc];
-%         [val , ind] = sort(tosort(1,:),'descend');
-%         sorted_table = tosort(:,ind);
-%         T_max = [];
-%         if length(sorted_table) > 2
-%             sorted_table = sorted_table(:,[1 2 ]);
-%         end
-%         
-% %         if(length(sorted_table) == 4)
-% %         
-% %         if(length(sorted_table) == 3)
-% %             amp = sorted_table(1,:);
-% %             ind = sorted_table(2,:);
-% %             if(ind(1) > round(0.13*Fs))
-% %                 if((ind(1) < ind(2)) && (ind(i) <ind(3))) 
-% %                     T_Max(i) = ind(1);
-% %                 else
-% %                     
-% %                 end
-% %             else
-% %                 
-% %             end
-%         amp = sorted_table(1,:);
-%         ind = sorted_table(2,:);  
-%         
-%         if(length(amp) == 2) 
-%           
-%             if((ind(1) <  ind(2)) && (ind(1) > round(0.1*Fs)) && ((ind(2) - ind(1)) < round(0.12*Fs) ))
-%                 T_Max(i) =R_index(i) + round(0.1*Fs) + ind(1);
-%             else
-%                 T_Max(i) =R_index(i) + round(0.1*Fs) + ind(2);
-%             end
-%             
-%         elseif(length(amp) == 1)
-%             T_Max(i) = R_index(i) + round(0.1*Fs) + sorted_table(2,1);            
-%         end
-% %         hold on
-% %         plot(new_loc,new_pks,'or');
-% figure();
-% plot(time,signal,time(startIndexes),signal(startIndexes),'or');
+legend('Signal filtered','R-Peak','QRS Onset','QRS End','T Max');
+   
+
+Fs = 1000;
+time_slope = []
+maxslope =[]
+for i=1:40
+    time_slope(i) =i;
+end
+switch(type)
+    case 'negative'
+        disp('T wave negative');
+       
+        for i=1:length(tmax)
+            maxslope = [];
+            ypoints = signal(tmax(i):tmax(i)+39);
+            ypoints_diff = diff(ypoints);
+            [y,indexes] = sort(ypoints_diff,'descend');
+            for j=1:20
+                maxslope(j) = tmax(i)+indexes(j);
+            end
+            yfit = signal(maxslope);
+           
+            yfit = yfit';            
+            maxSlope = [ones(length(maxslope'),1) maxslope'];
+            coeff = maxSlope\yfit;
+            T_End(i) = round(-1*(coeff(1))/coeff(2));
+%             figure()
+%             plot(time_slope,ypoints,time_slope(indexes(1,1:20)),ypoints(indexes(1,1:20)),'or');
+            
+        end
+            
+    case 'positive'
+
+            
+    otherwise
+        disp('Unknown method');
+        
+end
